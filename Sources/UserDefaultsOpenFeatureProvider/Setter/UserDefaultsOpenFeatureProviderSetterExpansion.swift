c5ef238ee18a72bcc4328dc1a4a6b3a326db7a49
Fumito Ito <fumito.ito@nex.nikkei.com>
2024-01-09 09:51:11 +0900

    add setter function

--- Sources/UserDefaultsOpenFeatureProvider/Setter/UserDefaultsOpenFeatureProviderSetterExpansion.swift
+++ Sources/UserDefaultsOpenFeatureProvider/Setter/UserDefaultsOpenFeatureProviderSetterExpansion.swift
@@ -10,54 +10,16 @@ import OpenFeature
 
 extension UserDefaultsOpenFeatureProvider {
     private static let jsonEncoder = JSONEncoder()
-
-    func setBooleanValue(forKey key: String, givenCondition condition: @escaping ((_ targetingKey: String) -> Bool), with context: EvaluationContext?) throws {
-        guard let context = [context, defaultContext].compactMap({ $0 }).first else {
-            throw OpenFeatureError.generalError(message: "`\(#function)` needs context or initial context to define targeting key")
-        }
-
-        let defaults = try getCurrentUserDefaults(with: context)
-        defaults.set(condition(context.getTargetingKey()), forKey: key)
-    }
-
-    func setStringValue(forKey key: String, givenCondition condition: @escaping ((_ targetingKey: String) -> String), with context: EvaluationContext?) throws {
-        guard let context = [context, defaultContext].compactMap({ $0 }).first else {
-            throw OpenFeatureError.generalError(message: "`\(#function)` needs context or initial context to define targeting key")
-        }
-
-        let defaults = try getCurrentUserDefaults(with: context)
-        defaults.set(condition(context.getTargetingKey()), forKey: key)
-    }
-
-    func setIntegerValue(forKey key: String, givenCondition condition: @escaping ((_ targetingKey: String) -> Int64), with context: EvaluationContext?) throws {
-        guard let context = [context, defaultContext].compactMap({ $0 }).first else {
-            throw OpenFeatureError.generalError(message: "`\(#function)` needs context or initial context to define targeting key")
-        }
-
-        let defaults = try getCurrentUserDefaults(with: context)
-        defaults.set(condition(context.getTargetingKey()), forKey: key)
-    }
-
-    func setDoubleValue(forKey key: String, givenCondition condition: @escaping ((_ targetingKey: String) -> Double), with context: EvaluationContext?) throws {
-        guard let context = [context, defaultContext].compactMap({ $0 }).first else {
-            throw OpenFeatureError.generalError(message: "`\(#function)` needs context or initial context to define targeting key")
-        }
-
-        let defaults = try getCurrentUserDefaults(with: context)
-        defaults.set(condition(context.getTargetingKey()), forKey: key)
-    }
-
-    func setDateValue(forKey key: String, givenCondition condition: @escaping ((_ targetingKey: String) -> Date), with context: EvaluationContext?) throws {
-        guard let context = [context, defaultContext].compactMap({ $0 }).first else {
-            throw OpenFeatureError.generalError(message: "`\(#function)` needs context or initial context to define targeting key")
-        }
-
-        let defaults = try getCurrentUserDefaults(with: context)
-        let date = condition(context.getTargetingKey())
-        defaults.set(date, forKey: key)
-    }
-
-    func setListValue(forKey key: String, givenCondition condition: @escaping ((_ targetingKey: String) -> [Any]), with context: EvaluationContext?) throws {
+    
+    /// Sets the Bool value to the provider's specified key as a flag
+    ///
+    /// If a value is set through this function, the `PROVIDER_CONFIGURATION_CHANGED` event is sent.
+    ///
+    /// - Parameters:
+    ///   - key: key to set value
+    ///   - condition: function to calculate the value to be set based on the targeting key given as an argument.
+    ///   - context: context for provider
+    public func setBooleanValue(forKey key: String, givenCondition condition: @escaping ((_ targetingKey: String) -> Bool), with context: EvaluationContext?) throws {
         guard let context = [context, defaultContext].compactMap({ $0 }).first else {
             throw OpenFeatureError.generalError(message: "`\(#function)` needs context or initial context to define targeting key")
         }
@@ -65,9 +27,18 @@ extension UserDefaultsOpenFeatureProvider {
         let defaults = try getCurrentUserDefaults(with: context)
         let value = condition(context.getTargetingKey())
         defaults.set(value, forKey: key)
+        emit(event: .configurationChanged, details: [key: value])
     }
 
-    func setStructureValue(forKey key: String, givenCondition condition: @escaping ((_ targetingKey: String) -> [String: Any]), with context: EvaluationContext?) throws {
+    /// Sets the String value to the provider's specified key as a flag
+    ///
+    /// If a value is set through this function, the `PROVIDER_CONFIGURATION_CHANGED` event is sent.
+    ///
+    /// - Parameters:
+    ///   - key: key to set value
+    ///   - condition: function to calculate the value to be set based on the targeting key given as an argument.
+    ///   - context: context for provider
+    public func setStringValue(forKey key: String, givenCondition condition: @escaping ((_ targetingKey: String) -> String), with context: EvaluationContext?) throws {
         guard let context = [context, defaultContext].compactMap({ $0 }).first else {
             throw OpenFeatureError.generalError(message: "`\(#function)` needs context or initial context to define targeting key")
         }
@@ -75,5 +46,103 @@ extension UserDefaultsOpenFeatureProvider {
         let defaults = try getCurrentUserDefaults(with: context)
         let value = condition(context.getTargetingKey())
         defaults.set(value, forKey: key)
+        emit(event: .configurationChanged, details: [key: value])
+    }
+
+    /// Sets the Int64 value to the provider's specified key as a flag
+    ///
+    /// If a value is set through this function, the `PROVIDER_CONFIGURATION_CHANGED` event is sent.
+    ///
+    /// - Parameters:
+    ///   - key: key to set value
+    ///   - condition: function to calculate the value to be set based on the targeting key given as an argument.
+    ///   - context: context for provider
+    public func setIntegerValue(forKey key: String, givenCondition condition: @escaping ((_ targetingKey: String) -> Int64), with context: EvaluationContext?) throws {
+        guard let context = [context, defaultContext].compactMap({ $0 }).first else {
+            throw OpenFeatureError.generalError(message: "`\(#function)` needs context or initial context to define targeting key")
+        }
+
+        let defaults = try getCurrentUserDefaults(with: context)
+        let value = condition(context.getTargetingKey())
+        defaults.set(value, forKey: key)
+        emit(event: .configurationChanged, details: [key: value])
+    }
+
+    /// Sets the Double value to the provider's specified key as a flag
+    ///
+    /// If a value is set through this function, the `PROVIDER_CONFIGURATION_CHANGED` event is sent.
+    ///
+    /// - Parameters:
+    ///   - key: key to set value
+    ///   - condition: function to calculate the value to be set based on the targeting key given as an argument.
+    ///   - context: context for provider
+    public func setDoubleValue(forKey key: String, givenCondition condition: @escaping ((_ targetingKey: String) -> Double), with context: EvaluationContext?) throws {
+        guard let context = [context, defaultContext].compactMap({ $0 }).first else {
+            throw OpenFeatureError.generalError(message: "`\(#function)` needs context or initial context to define targeting key")
+        }
+
+        let defaults = try getCurrentUserDefaults(with: context)
+        let value = condition(context.getTargetingKey())
+        defaults.set(value, forKey: key)
+        emit(event: .configurationChanged, details: [key: value])
+    }
+
+    /// Sets the Date value to the provider's specified key as a flag
+    ///
+    /// If a value is set through this function, the `PROVIDER_CONFIGURATION_CHANGED` event is sent.
+    ///
+    /// - Parameters:
+    ///   - key: key to set value
+    ///   - condition: function to calculate the value to be set based on the targeting key given as an argument.
+    ///   - context: context for provider
+    public func setDateValue(forKey key: String, givenCondition condition: @escaping ((_ targetingKey: String) -> Date), with context: EvaluationContext?) throws {
+        guard let context = [context, defaultContext].compactMap({ $0 }).first else {
+            throw OpenFeatureError.generalError(message: "`\(#function)` needs context or initial context to define targeting key")
+        }
+
+        let defaults = try getCurrentUserDefaults(with: context)
+        let value = condition(context.getTargetingKey())
+        defaults.set(value, forKey: key)
+        emit(event: .configurationChanged, details: [key: value])
+    }
+
+    /// Sets the List value to the provider's specified key as a flag
+    ///
+    /// If a value is set through this function, the `PROVIDER_CONFIGURATION_CHANGED` event is sent.
+    /// The values in this array must be values that can be converted to `OpenFeature.Value`.
+    ///
+    /// - Parameters:
+    ///   - key: key to set value
+    ///   - condition: function to calculate the value to be set based on the targeting key given as an argument.
+    ///   - context: context for provider
+    public func setListValue(forKey key: String, givenCondition condition: @escaping ((_ targetingKey: String) -> [Any]), with context: EvaluationContext?) throws {
+        guard let context = [context, defaultContext].compactMap({ $0 }).first else {
+            throw OpenFeatureError.generalError(message: "`\(#function)` needs context or initial context to define targeting key")
+        }
+
+        let defaults = try getCurrentUserDefaults(with: context)
+        let value = condition(context.getTargetingKey())
+        defaults.set(value, forKey: key)
+        emit(event: .configurationChanged, details: [key: value])
+    }
+
+    /// Sets the Dictionary value to the provider's specified key as a flag
+    ///
+    /// If a value is set through this function, the `PROVIDER_CONFIGURATION_CHANGED` event is sent.
+    /// The values in this dictionary must be values that can be converted to `OpenFeature.Value`.
+    ///
+    /// - Parameters:
+    ///   - key: key to set value
+    ///   - condition: function to calculate the value to be set based on the targeting key given as an argument.
+    ///   - context: context for provider
+    public func setStructureValue(forKey key: String, givenCondition condition: @escaping ((_ targetingKey: String) -> [String: Any]), with context: EvaluationContext?) throws {
+        guard let context = [context, defaultContext].compactMap({ $0 }).first else {
+            throw OpenFeatureError.generalError(message: "`\(#function)` needs context or initial context to define targeting key")
+        }
+
+        let defaults = try getCurrentUserDefaults(with: context)
+        let value = condition(context.getTargetingKey())
+        defaults.set(value, forKey: key)
+        emit(event: .configurationChanged, details: [key: value])
     }
 }
